d7ad0efa43cdd6a130644d461c8f4d9be3546946
NR5P <orangev8zcar@gmail.com>
2019-05-26 18:54:26 -0500

    adding images to repo and started working on the javascript file

---
+++ assign06.js
@@ -0,0 +1,13 @@
+const submit = document.getElementsByName("validate")[0];
+const reset = document.getElementById("reset");
+
+///////////////////////ONCLICK LISTENERS////////////////////////////
+submit.onclick(function() {
+
+})
+
+reset.onclick(function() {
+
+})
+
+//////////////////////END ONCLICK LISTENERS/////////////////////////
